b21058901b455326ecbca49089fe733304ca4e77
itzcookiie <mayowaadeniyi@hotmail.com>
2020-02-15 21:45:59 +0000

    app set up with react, webpack and babel

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path')
+
+module.exports = {
+    devtool: 'inline-source-map',
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    devServer: {
+        contentBase: path.resolve(__dirname, 'public'),
+        port: 3000,
+        publicPath: '/dist/',
+        watchContentBase: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-env', '@babel/preset-react']
+                }
+            }
+        ]
+    }
+}
